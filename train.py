aded05ed7fab908330b73acedbfbc5e0177e960f
Ioana Simion <scur0400@int5.local.snellius.surf.nl>
2024-05-01 23:44:37 +0200

    added dataloader to train script

--- train.py
+++ train.py
@@ -1,3 +1,44 @@
+import argparse
+from qm9.dataset import retrieve_dataloaders
+
+parser = argparse.ArgumentParser(description='QM9 Example')
+parser.add_argument('--exp_name', type=str, default='exp_1', metavar='N',
+                    help='experiment_name')
+parser.add_argument('--batch_size', type=int, default=96, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=1000, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log_interval', type=int, default=20, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--test_interval', type=int, default=1, metavar='N',
+                    help='how many epochs to wait before logging test')
+parser.add_argument('--outf', type=str, default='qm9/logs', metavar='N',
+                    help='folder to output vae')
+parser.add_argument('--lr', type=float, default=1e-3, metavar='N',
+                    help='learning rate')
+parser.add_argument('--property', type=str, default='homo', metavar='N',
+                    help='label to predict: alpha | gap | homo | lumo | mu | Cv | G | H | r2 | U | U0 | zpve')
+parser.add_argument('--num_workers', type=int, default=0, metavar='N',
+                    help='number of workers for the dataloader')
+parser.add_argument('--charge_power', type=int, default=2, metavar='N',
+                    help='maximum power to take into one-hot features')
+parser.add_argument('--weight_decay', type=float, default=1e-16, metavar='N',
+                    help='weight decay')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+device = torch.device("cuda" if args.cuda else "cpu")
+
+
+
+def train():
+    # Get dataloaders and charge scale for QM9
+    dataloaders, charge_scale = retrieve_dataloaders(args.batch_size, args.num_workers)
+
 if __name__ == "__main__":
 
     print("Train")
